702713f98873342ca3bf504ba0d08fa8709a1e89
Manuel Laggner <manuel.laggner@gmail.com>
2026-04-26 15:51:39 +0200

    ✨ [HTTP] added a browser based url fetching logic

---
+++ src/test/java/org/tinymediamanager/scraper/http/BrowserUrlTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012 - 2026 Manuel Laggner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tinymediamanager.scraper.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.tinymediamanager.scraper.util.UrlUtil;
+import org.tinymediamanager.scraper.util.UrlUtil.UrlImpl;
+
+/**
+ * The class {@link BrowserUrlTest} validates browser-backed URL fetching.
+ *
+ * @author Manuel Laggner
+ */
+public class BrowserUrlTest {
+
+  /**
+   * Test parsing a JavaScript-capable URL implementation through {@link UrlUtil}.
+   *
+   * @throws Exception
+   *           if browser navigation fails
+   */
+  @Test
+  public void testGetStringFromUrlViaBrowser() throws Exception {
+    String html = UrlUtil.getStringFromUrl("https://example.com", UrlImpl.BROWSER);
+
+    assertThat(html).isNotEmpty();
+    assertThat(html.toLowerCase()).contains("example");
+  }
+}
