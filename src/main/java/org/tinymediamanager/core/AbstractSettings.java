9d93ad5c4130304b70dc694f63860f0e0f28783c
Manuel Laggner <manuel.laggner@gmail.com>
2026-05-04 12:55:08 +0200

    ♻️ [Settings] write settings with try-with-resources

--- src/main/java/org/tinymediamanager/core/AbstractSettings.java
+++ src/main/java/org/tinymediamanager/core/AbstractSettings.java
@@ -186,26 +186,13 @@ public abstract class AbstractSettings extends AbstractModelObject {
     }
 
     // write as JSON
-    Writer writer = null;
-    try {
+    try (Writer writer = new FileWriter(new File(getSettingsFolder(), getConfigFilename()))) {
       String settings = objectWriter.writeValueAsString(this);
-      writer = new FileWriter(new File(getSettingsFolder(), getConfigFilename()));
       IOUtils.write(settings, writer);
     }
     catch (Exception e) {
       getLogger().error("saveSettings", e);
       MessageManager.getInstance().pushMessage(new Message(Message.MessageLevel.ERROR, "tmm.settings", "message.config.savesettingserror"));
-    }
-    finally {
-      if (writer != null) {
-        try {
-          writer.close();
-        }
-        catch (Exception e) {
-          getLogger().error("saveSettings", e);
-          MessageManager.getInstance().pushMessage(new Message(Message.MessageLevel.ERROR, "tmm.settings", "message.config.savesettingserror"));
-        }
-      }
     }
 
     // clear dirty flag
